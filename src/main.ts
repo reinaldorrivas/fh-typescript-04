5252a62dfac732de7b4f30e17d13e8712065ea9f
René Rivas <reinaldorrivas@gmail.com>
2026-02-06 12:38:50 -0400

    fh-ts-guide: tsconfig intro init

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = /* html */ `
+  <div>
+    <h1>Depuración de errores y explicación de tsconfig.json</h1>
+  </div>
+`;
